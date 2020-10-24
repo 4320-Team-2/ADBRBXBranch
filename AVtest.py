67b69d8b29f06f987ee331e5f3eea84c98a3a1b5
Andrew Brown <35886726+Tengoon@users.noreply.github.com>
2020-10-24 14:41:56 -0500

    Update AVtest.py
    Fixed parsing, now working on graphing the data.

--- AVtest.py
+++ AVtest.py
@@ -5,14 +5,6 @@ import pygal
 #URL to Alphavantage API
 API_URL = "https://www.alphavantage.co/query"
 
-#Time Series Selection, ignore.
-#print("Please Select a Time Series")
-#print("1.) Intradaily")
-#print("2.) Daily")
-#print("3.) Weekly")
-#print("4.) Monthly")
-#seriesSelection = input("Please enter a selection (1, 2, 3, 4): ")
-
 #Selecting the time series function, company symbol, and how big the output size will be, and our API key.
 data = {
     "function":"TIME_SERIES_DAILY",
@@ -27,64 +19,37 @@ apiCall = rq.get(API_URL, params=data)
 #Stores the json-enconded content in the retrieved data.
 response = apiCall.json()
 
-#Print tests to make sure the response is correct.
-#print(response.keys())
-#print(response.values())
-#print(response)
-
-#Test print that shows a pretty output of the json, it'll give you a good idea how the data structure looks.
-#print(json.dumps(response, indent=4))
-
-#Testing to see if the JSON file recived from the apiCall works by writing a local file.
-#If you see how the data strcuture looks, the remove the "#" from the next three lines and run the code.
-#f = open("test.txt","w")
-#f.write(repr(response))
-#f.close()
 
 #Retriving the dates in putting them into a dictonary.
 dates = []
 [dates.append(date) for date in response["Time Series (Daily)"]]
 #print(dates)
 
-#I was delirous when I wrote this, I can't explain what I was doing here.
-#key = tuple(dates)
-#doubleDate = []
-#for x in range(len(key)):
-#	[doubleDate.append(day[key]) for day in dates]
-#print(doubleDate)
-
-#starts is a dictonary of the opening prices for the stock.
-starts = []
-
-#An implentation of trying to put the opening prices into the dict. I don't remeber what I was doing here either, ignore for now
-#for date in response["Time Series (Daily)"]:
-#	for opening in response["Time Series (Daily)"][date]["1. open"]:
-#		[starts.append(opening)]
-
-
 #Creating a tuple of the dates so we can use them as key.
 key = tuple(dates)
 
-#this is suppose to access the opening prices and put them into a dictonary
-#the first loop basically assings the first date found
-#the second loop then access the opening prices and appends them into the starts dictonary
-#however this is where my problem starts. Instead of appending the whole price number, it instead splits it up individually
-#for example, "110.155" is instead appended as "1" "1" "0" "." "1" "5" "5"
-#i have no clue how to make it work as intended
+opens = []
 for x in range(len(key)):
-	for opening in response["Time Series (Daily)"][key[x]]["1. open"]:
-		[starts.append(opening)]
-print(starts)
+    opens.append(response["Time Series (Daily)"][key[x]]["1. open"])
+    
+highs = []
+for x in range(len(key)):
+    highs.append(response["Time Series (Daily)"][key[x]]["2. high"])
+    
+lows = []
+for x in range(len(key)):
+    lows.append(response["Time Series (Daily)"][key[x]]["3. low"])
+    
+closes = []
+for x in range(len(key)):
+    closes.append(response["Time Series (Daily)"][key[x]]["4. close"])
+    
+#Dumb Stuff Down Here, converting list of strings to list of floats
+test = [float(i) for i in opens]
 
-#My really hacky solution that doesn't actually work at all, ignore for now.
-#price = ""
-#for x in range(len(key)):
-#        for opening in response["Time Series (Daily)"][key[x]]["1. open"]:
-#                price += opening
-#tests = ' '.join(price[i:i + 8] for i in range(0, len(price), 8))
 
-#put the dict infomartion into graphs and rendering them, not fully implemented so ignore for now. 
-#bar_chart = pygal.Line()
-#bar_chart.x_labels = dates
-#bar_chart.render_to_file("populationviz.svg")
-#print("Success!")
+bar_chart = pygal.Line()
+bar_chart.x_labels = dates
+bar_chart.add("open",test)
+bar_chart.render_to_file("testing.svg")
+print("Success!")
