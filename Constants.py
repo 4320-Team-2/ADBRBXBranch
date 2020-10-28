c68b7040efce1e75ced2e527fbb15e6d86095070
Andrew Brown <35886726+Tengoon@users.noreply.github.com>
2020-10-28 14:24:35 -0500

    Add files via upload
    Additional comments and tweaks to constants.

--- Constants.py
+++ Constants.py
@@ -1,8 +1,9 @@
 API_URL = "https://www.alphavantage.co/query"
+API_KEY = "SPFP6JN8KMDZ9IPM"
 #Constants below this comment will be set by user input and currently hold placeholder values.
+#See https://www.alphavantage.co/documentation/#daily for how the function parameters work. 
 SYMBOL = "GOOGL"
 CHART_TYPE = True
-TIME_SERIES = "TIME_SERIES_WEEKLY"
-API_KEY = "SPFP6JN8KMDZ9IPM"
+TIME_SERIES = "TIME_SERIES_DAILY"
 START_DATE = "2020-01-01"
 END_DATE = "2020-01-31"
