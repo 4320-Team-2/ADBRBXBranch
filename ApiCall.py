5d91b706fc61e33f4cb031ad7fa361e819824693
Andrew Brown <35886726+Tengoon@users.noreply.github.com>
2020-10-28 02:23:00 -0500

    Add files via upload
    Cleaned up some code, fixed up different time series keys.

--- ApiCall.py
+++ ApiCall.py
@@ -10,6 +10,7 @@ data = {
     "function":Constants.TIME_SERIES,
     "symbol":Constants.SYMBOL,
     "outputsize":"full",
+    "interval":"60min",
     "apikey":Constants.API_KEY
     }
 
@@ -19,9 +20,18 @@ apiCall = rq.get(Constants.API_URL, params=data)
 #Stores the json-enconded content in the retrieved data.
 response = apiCall.json()
 
+# If statements to change key depending on the Time Serires selected. 
+if Constants.TIME_SERIES == "TIME_SERIES_DAILY":
+    timeSeries = "Time Series (Daily)"
+elif Constants.TIME_SERIES == "TIME_SERIES_WEEKLY":
+    timeSeries = "Weekly Time Series"
+elif Constants.TIME_SERIES == "TIME_SERIES_MONTHLY":
+    timeSeries = "Monthly Time Series"
+    
+    
 #Retriving the dates in putting them into a dictonary.
 dates = []
-[dates.append(date) for date in response["Time Series (Daily)"]]
+[dates.append(date) for date in response[timeSeries]]
 
 #Making a set out of date so we can find its intersections.
 set_2 = frozenset(date.date_strings)
@@ -38,19 +48,19 @@ key = tuple(final_dates)
 #Populating lists with data from API
 opens = []
 for x in range(len(key)):
-    opens.append(response["Time Series (Daily)"][key[x]]["1. open"])
+    opens.append(response[timeSeries][key[x]]["1. open"])
     
 highs = []
 for x in range(len(key)):
-    highs.append(response["Time Series (Daily)"][key[x]]["2. high"])
+    highs.append(response[timeSeries][key[x]]["2. high"])
     
 lows = []
 for x in range(len(key)):
-    lows.append(response["Time Series (Daily)"][key[x]]["3. low"])
+    lows.append(response[timeSeries][key[x]]["3. low"])
     
 closes = []
 for x in range(len(key)):
-    closes.append(response["Time Series (Daily)"][key[x]]["4. close"])
+    closes.append(response[timeSeries][key[x]]["4. close"])
     
 #Dumb Stuff Down Here, converting list of strings to list of floats so we can plot them
 floatOpen = [float(i) for i in opens]
--- Constants.py
+++ Constants.py
@@ -1,8 +1,8 @@
-#Where we keep all our Constants at. 
 API_URL = "https://www.alphavantage.co/query"
+#Constants below this comment will be set by user input and currently hold placeholder values.
 SYMBOL = "GOOGL"
 CHART_TYPE = True
-TIME_SERIES = "TIME_SERIES_DAILY"
+TIME_SERIES = "TIME_SERIES_WEEKLY"
 API_KEY = "SPFP6JN8KMDZ9IPM"
-START_DATE = "2020-08-01"
-END_DATE = "2020-08-31"
+START_DATE = "2020-01-01"
+END_DATE = "2020-01-31"
